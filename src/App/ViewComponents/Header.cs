433a523c01681aa4db17b503a6b6177572a3910b
Peter Elliott <pelliott@official-sounding.com>
2025-06-09 16:11:49 -0500

    styling for post page

--- src/App/ViewComponents/Header.cs
+++ src/App/ViewComponents/Header.cs
@@ -1,14 +1,16 @@
 using App.Models;
+using Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.ViewComponents;
 
 
-public class Header : ViewComponent
+public class Header(DataContext context) : ViewComponent
 {
     public IViewComponentResult Invoke()
     {
         var subsite = Request.Host.Host.Split('.')[0] ?? "www";
-        return View(new HeaderModel(subsite, User.Identity?.IsAuthenticated ?? false, User.Identity?.Name));
+        var site = context.Sites.Single(s => s.Slug == subsite);
+        return View(new HeaderModel(site, User.Identity?.IsAuthenticated ?? false, User.Identity?.Name));
     }
 }
