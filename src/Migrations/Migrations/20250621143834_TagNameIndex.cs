60f4afafc4019edee70a33b3725319d33398da1d
Peter Elliott <pelliott@official-sounding.com>
2025-06-21 10:48:48 -0500

    Tag Detail page

---
+++ src/Migrations/Migrations/20250621143834_TagNameIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagNameIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "idx_tag_name",
+                table: "Tag",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "idx_tag_name",
+                table: "Tag");
+        }
+    }
+}
