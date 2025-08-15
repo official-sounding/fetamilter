a553017f8e513020a4814a6b2b4073cce4d7a74c
P. Elliott <pelliott@official-sounding.com>
2025-08-15 12:34:32 -0500

    Fix issues with config domain specification
    - login fails when port is specified in Set-Cookie domain attribute
    - Build Uris using UriBuilder instead of string concatenation

--- src/App/Config/SiteConfig.cs
+++ src/App/Config/SiteConfig.cs
@@ -4,5 +4,17 @@ public class SiteConfig
 {
     public const string SECTION = "Site";
     public string? RootDomain { get; set; }
+    public int? Port { get; set; }
     public bool UseHttps { get; set; }
+
+    public Uri BuildUri(string subDomain) {
+
+            var builder = new UriBuilder() { Scheme = UseHttps ? "https" : "http", Host = $"{subDomain}.{RootDomain}" };
+            if (Port.HasValue)
+            {
+                builder.Port = Port.Value;
+            }
+
+            return builder.Uri;
+    }
 }
